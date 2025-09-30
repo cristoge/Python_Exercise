1494e69bc1dd4d81626bc67d83289d19430a57b4
cristoge <cristo.g@outlook.es>
2025-09-30 20:05:00 +0200

    ej Reinas, poblacion creada

--- exercises/reinas.py
+++ exercises/reinas.py
@@ -10,3 +10,35 @@
 #
 # Utilitza els apunts de classe per tal de realitzar tots els passos d’un algoritme genetic:
 # 	reproducció, mutació, selecció…
+#
+#
+
+import random
+
+"""
+Generacion de poblacion
+"""
+
+
+def generar_individuo(medida: int):
+    nuevo_individuo = list(range(medida))
+    random.shuffle(nuevo_individuo)
+    return nuevo_individuo
+
+
+def generar_poblacion(medida_tablero: int, medida_poblacion: int):
+    poblacion = []
+    for _ in range(medida_poblacion):
+        individuo = generar_individuo(medida_tablero)
+        poblacion.append(individuo)
+    print(poblacion)
+    return poblacion
+
+
+generar_poblacion(8, 4)
+
+
+"""
+Ahora tocaria definir la fitness
+empezariamos con una lista vacias de colision
+"""
