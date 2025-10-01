a7ac28b55c703f15a0d1f9ad0fc1844aa199223a
cristoge <cristo.g@outlook.es>
2025-10-01 19:13:59 +0200

    pasos descritos

--- exercises/reinas.py
+++ exercises/reinas.py
@@ -10,8 +10,8 @@
 #
 # Utilitza els apunts de classe per tal de realitzar tots els passos d’un algoritme genetic:
 # 	reproducció, mutació, selecció…
-#
-#
+# consejos, diversity score,
+# usar una seed
 
 import random
 
@@ -20,25 +20,54 @@ Generacion de poblacion
 """
 
 
-def generar_individuo(medida: int):
+def generar_individuo(medida: int) -> list:
     nuevo_individuo = list(range(medida))
     random.shuffle(nuevo_individuo)
     return nuevo_individuo
 
 
-def generar_poblacion(medida_tablero: int, medida_poblacion: int):
+def generar_poblacion(medida_tablero: int, medida_poblacion: int) -> list:
     poblacion = []
     for _ in range(medida_poblacion):
         individuo = generar_individuo(medida_tablero)
         poblacion.append(individuo)
-    print(poblacion)
     return poblacion
 
 
-generar_poblacion(8, 4)
+poblacion = generar_poblacion(8, 4)
+print(poblacion)
+
+"""
+Funcion para buscar los mejores
+"""
+
+
+def fitness(poblacion: list):
+    pass
 
 
 """
-Ahora tocaria definir la fitness
-empezariamos con una lista vacias de colision
+Aqui llamamos a la funcion de fitness para seleccionar a los mejores individuos y ordenamos.
 """
+
+
+def seleccion(poblacion: list) -> list:
+    pass
+
+
+"""
+coger todos los padres y de dos en dos van teniendo hijos
+"""
+
+
+def crosover(poblacion: list) -> list:
+    pass
+
+
+"""
+Funcion para reproducir_dos_padres
+"""
+
+
+def reproducir_dos_padres(padre1: list, padre2: list):
+    pass
